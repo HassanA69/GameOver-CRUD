351b804a6fad4eec564a5bcdbaaab2a0c7581c79
Hassan Abdelnaby <hassan.abdelnaby69@gmail.com>
2025-09-19 01:09:42 +0300

    feat: Add Games Controller with CRUD functionality

---
+++ GameOver/Settings/FileSettings.cs
@@ -0,0 +1,9 @@
+namespace GameOver.Settings;
+
+public static class FileSettings
+{
+    public const string ImagesPath = "/assets/images/games";
+    public const string AllowedExtensions = ".jpg,.jpeg,.png";
+    public const int MaxFileSizeInMB = 1;
+    public const int MaxFileSizeInByte = MaxFileSizeInMB * 1024  * 1024;
+}
